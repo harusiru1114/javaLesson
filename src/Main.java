b7cea81d4f0e750f8a9b2d2d5795514a774b73f0
Jeong Chanwoo <heemangchanwoo@gmail.com>
2025-08-12 22:52:51 +0900

    시루시루시루시루

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("하루시루가 제일 귀여워");
         System.out.println("너무 귀여워");
+        System.out.println("시루시루시루시루시루");
     }
 }
