a09f4cdda620175c9c1aa23682c45f77a25615b4
Jeong Chanwoo <heemangchanwoo@gmail.com>
2025-08-12 22:36:01 +0900

    하루시루가 귀여움을 나타냈음

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("하루시루가 제일 귀여워");
+        System.out.println("너무 귀여워");
+    }
+}
